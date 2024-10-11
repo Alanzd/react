4773d9bc99a5741004d354cc9b83f72ddbea3047
Ana_Lanz <ana.lanz.d@gmail.com>
2024-10-11 14:15:43 +0200

    wip

--- pokedex/src/components/Pokemones.jsx
+++ pokedex/src/components/Pokemones.jsx
@@ -7,8 +7,8 @@ function Pokemon({ id, pokename, image }) {
     <div className='pokemon-card'>
       <img alt={pokename} src={image} className='pokemon-image' />
       <p className='pokemon-title'>
-        <span>{pokename}</span>
         <span>#{id}</span>
+        <span>{pokename}</span>
       </p>
     </div>
   )
@@ -24,20 +24,23 @@ const Pokemones = () => {
     const getPokemones = async () => {
       //recuperamos listado de pokemones
       const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0');
-
+     
       // como es una promesa tenemos que poner await en la respuesta y async en la funciom
       const listaPokemones = await response.json();
       const { results } = listaPokemones;
 
       const pokemones = results.map(async (pokemon) => {
         const response = await fetch(pokemon.url)
-        const poke = await response.json()
+        const poke = await response.json();
+        console.log(poke);
         return {
           id: poke.id,
           pokename: poke.name,
-          image: poke.sprites.front_default
+          image: poke.sprites.other.dream_world.front_default
         }
       })
+    
+      
       setPokemones(await Promise.all(pokemones))
     }
     getPokemones()
@@ -54,9 +57,11 @@ const Pokemones = () => {
         La función que le paso como argumento se ejecutará una vez por cada elemento del array pokemones.
         El resultado de .map() será un nuevo array con los elementos transformados por la función.
 
-        2. pokemon => <Pokemon {...pokemon} /> es un arrow function que toma un elemento del array pokemones (llamado pokemon). pokemon es el objeto actual en el array en cada iteración.
+        2. pokemon => <Pokemon {...pokemon} /> es un arrow function que toma un elemento del array pokemones (llamado pokemon). 
+        pokemon es el objeto actual en el array en cada iteración.
 
-        3.<Pokemon {...pokemon} /> El operador spread (...) se usa para "expandir" un objeto. En este caso, lo que hace es tomar todas las propiedades del objeto pokemon y pasarlas como props al componente Pokemon.
+        3.<Pokemon {...pokemon} /> El operador spread (...) se usa para "expandir" un objeto. 
+        En este caso, lo que hace es tomar todas las propiedades del objeto pokemon y pasarlas como props al componente Pokemon.
         esto es equivalente a escribir: <Pokemon id={1} name="loquesea" image="loquesea" /> */
 
       }
