bc80f131c76fddb55bf6aef83c6542889bc78344
Ana_Lanz <ana.lanz.d@gmail.com>
2024-10-16 16:21:02 +0200

    css icons

--- pokedex/src/components/Footer.tsx
+++ pokedex/src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import styles from './footer.module.css'
 import { Link } from 'react-router-dom'
 import PokemonPic from '../assets/images/pikachu.png';
 import LocationPic from '../assets/images/pokeball.png';
@@ -6,17 +7,17 @@ import ItemPic from '../assets/images/location.png';
 
 export const Footer = () => {
   return (
-    <footer>
-    <Link to="/pokemons">
-      <img src={PokemonPic} alt="pokeball" />
+    <footer className={styles.footer}>
+    <Link to="/pokemons" className='footerLink'>
+      <img className='footerIcon' src={PokemonPic} alt="pokeball" />
       Pokemons
     </Link>
-    <Link to="/pokemons">
-      <img src={LocationPic} alt="pokeball" />
+    <Link className='footerLink' to="/pokemons">
+      <img className='footerIcon' src={LocationPic} alt="pokeball" />
       Items
     </Link>
-    <Link to="/pokemons">
-      <img src={ItemPic} alt="pokeball" />
+    <Link className='footerLink' to="/pokemons">
+      <img className='footerIcon' src={ItemPic} alt="pokeball" />
       Map
     </Link>
   </footer>
